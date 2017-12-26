41a501c4a9b2147106b77a4f50482f0e624a4caf
Unknown <samueldrouin@icloud.com>
2017-12-26 12:32:16 -0500

    Updated mainwindow UI

--- mainwindow.py
+++ mainwindow.py
@@ -1,20 +1,7 @@
 # Python import
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTableWidget
 import os
-import pymysql.cursors
-
-# Project import
-from consultation import Consultation
-import place
-
-# Variable
-g_host = 'localhost'
-g_user = 'samueldrouin'
-g_password = 'XqB4ao9yj2c9NPsKaWz2qTR1C9hLtU4e'
-g_db = 'Memory'
-g_charset = 'utf8'
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,65 +9,98 @@ class MainWindow(QMainWindow):
         ui = os.path.join(os.path.dirname(__file__),'UI','mainwindow.ui')
         uic.loadUi(ui, self)
 
-        # Slots
-        self.btn_consult_participantes.clicked.connect(self.open_consult_participantes)
-        self.btn_consult_activites.clicked.connect(self.open_consult_activite)
-        self.btn_consult_lieux.clicked.connect(self.open_consult_lieux)
-        self.btn_consult_membres.clicked.connect(self.open_consult_membres)
-        self.btn_consult_type_activite.clicked.connect(self.open_consult_type_activite)
-        self.btn_gest_lieux.clicked.connect(self.open_gest_lieux)
-
         # Actions
-        self.act_consult_membres.triggered.connect(self.open_consult_membres)
+        self.act_consult_participantes.triggered.connect(self.open_consult_participantes)
         self.act_consult_activites.triggered.connect(self.open_consult_activite)
         self.act_consult_lieux.triggered.connect(self.open_consult_lieux)
-        self.act_consult_participantes.triggered.connect(self.open_consult_participantes)
         self.act_consult_type_activite.triggered.connect(self.open_consult_type_activite)
 
     def open_consult_participantes(self):
         """
-        Ouvrir la fenêtre de consultation des participantes
+        Affiche le widget des options de tri pour les participantes
+        Affiche la liste des participantes correspondant aux options de tri
         :return: Aucun
         """
-        self.consultation = Consultation(4)
-        self.consultation.show()
-
-    def open_consult_membres(self):
-        """
-        Ouvrir la fenêtre de consultation des membres
-        :return: Aucun
-        """
-        self.consultation = Consultation(1)
-        self.consultation.show()
+        central_widget = CentralWidgetParticipantes()
+        self.setCentralWidget(central_widget)
 
     def open_consult_activite(self):
         """
-        Ouvrir la fenêtre de consultation des activites
+        Affiche le widget des options de tri pour les activités
+        Affiche la liste des activités correspondant aux options de tri
         :return: Aucun
         """
-        self.consultation = Consultation(2)
-        self.consultation.show()
-
-    def open_consult_lieux(self):
-        """
-        Ouvrir la fenêtre de consultation des lieux
-        :return: Aucun
-        """
-        self.consultation = Consultation(3)
-        self.consultation.show()
+        central_widget = CentralWidgetActivite()
+        self.setCentralWidget(central_widget)
 
     def open_consult_type_activite(self):
         """
-        Ouvrir la fenêtre de consultation des types d'activite
+        Affiche le widget des options de tri pour les types d'activites
+        Affiche la liste des types d'activites correspondant aux options de tri
         :return: Aucun
         """
-        self.consultation = Consultation(5)
-        self.consultation.show()
+        central_widget = CentralWidgetTypeActivite()
+        self.setCentralWidget(central_widget)
 
-    def open_gest_lieux(self):
+    def open_consult_lieux(self):
         """
-        Ouvrir la fenêtre de gestion des lieux
-        :return: Aucune
+        Affiche le widget des options de tri pour les lieux
+        Affiche la liste des lieux correspondant aux options de tri
+        :return:
         """
-        self.create_place = place.CreatePlace()
-        self.create_place.exec()
+        central_widget = CentralWidgetLieux()
+        self.setCentralWidget(central_widget)
+
+
+class CentralWidget(QWidget):
+    def __init__(self):
+        super(CentralWidget, self).__init__()
+        self.layout = QVBoxLayout(self)
+
+
+class CentralWidgetParticipantes(CentralWidget):
+    def __init__(self):
+        super(CentralWidgetParticipantes, self).__init__()
+        self.top_widget = QWidget()
+        ui = os.path.join(os.path.dirname(__file__), 'UI', 'widget_participantes.ui')
+        uic.loadUi(ui, self.top_widget)
+        self.layout.addWidget(self.top_widget)
+
+        self.table_widget = QTableWidget()
+        self.layout.addWidget(self.table_widget)
+
+
+class CentralWidgetActivite(CentralWidget):
+    def __init__(self):
+        super(CentralWidgetActivite, self).__init__()
+        self.top_widget = QWidget()
+        ui = os.path.join(os.path.dirname(__file__), 'UI', 'widget_activite.ui')
+        uic.loadUi(ui, self.top_widget)
+        self.layout.addWidget(self.top_widget)
+
+        self.table_widget = QTableWidget()
+        self.layout.addWidget(self.table_widget)
+
+
+class CentralWidgetLieux(CentralWidget):
+    def __init__(self):
+        super(CentralWidgetLieux, self).__init__()
+        self.top_widget = QWidget()
+        ui = os.path.join(os.path.dirname(__file__), 'UI', 'widget_lieux.ui')
+        uic.loadUi(ui, self.top_widget)
+        self.layout.addWidget(self.top_widget)
+
+        self.table_widget = QTableWidget()
+        self.layout.addWidget(self.table_widget)
+
+
+class CentralWidgetTypeActivite(CentralWidget):
+    def __init__(self):
+        super(CentralWidgetTypeActivite, self).__init__()
+        self.top_widget = QWidget()
+        ui = os.path.join(os.path.dirname(__file__), 'UI', 'widget_type_activite.ui')
+        uic.loadUi(ui, self.top_widget)
+        self.layout.addWidget(self.top_widget)
+
+        self.table_widget = QTableWidget()
+        self.layout.addWidget(self.table_widget)
