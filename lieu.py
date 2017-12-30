818187c4b959dc570d837e5355450ed0c31f6b6e
Unknown <samueldrouin@icloud.com>
2017-12-30 12:13:38 -0500

    Ajout des validator and completer a l'interface graphique du lieu

--- lieu.py
+++ lieu.py
@@ -15,6 +15,16 @@ class Lieu(Form):
         # Instance variable definition
         self.database = None
 
+        # Validator
+        self.txt_nom.setValidator(self.address_validator())
+        self.txt_adresse1.setValidator(self.address_validator())
+        self.txt_adresse2.setValidator(self.address_validator())
+        self.txt_ville.setValidator(self.name_validator())
+        self.txt_code_postal.setValidator(self.zip_code_validator())
+
+        # Completer
+        self.txt_ville.setCompleter(self.ville_completer())
+
         # Slots
         self.btn_cancel.clicked.connect(self.reject)
 
@@ -26,10 +36,18 @@ class NouveauLieu(Lieu):
         # Instance variable definition
         self.database = database
 
+        # Interface graphique
+        self.setWindowTitle("Nouveau lieu")
+        self.lbl_titre.setText("Nouveau lieu")
+
 
 class ModifierLieu(Lieu):
-    def __init__(self, database):
+    def __init__(self, database, id_lieu):
         super(ModifierLieu, self).__init__()
 
         # Instance variable definition
         self.database = database
+
+        # Interface graphique
+        self.setWindowTitle("Modifier un lieu")
+        self.lbl_titre.setText("Modifier un lieu")
