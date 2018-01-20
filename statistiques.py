cee18ab2ab48bdabc12ced1f217c2024e3623dca
samueldrouin <samueldrouin@icloud.com>
2018-01-20 09:17:41 -0500

    Amélioration des listes de champs de la base de donnée

--- statistiques.py
+++ statistiques.py
@@ -34,6 +34,157 @@ class Statistiques(Form):
     LISTE_CONTRAINTE_MEME_TABLE = ["et"]
     LISTE_CONTRAINTE_AUTRE_TABLE = ["et", "et exclusif", "ou"]
 
+    # Dictionnaire des tables pertinentes de la base de donnée
+    #   Key : Nom de la table
+    #   Value : Nom pour l'affichage
+    DICT_TABLE = {
+                  'activite' : {'nom' : 'Activité', 'reference' : ['activite', 'categorie_activite']},
+                  'article' : {'nom' : 'Article', 'reference' : ['article', 'facture', 'activite']},
+                  'categorie_activite' : {'nom' : 'Catégorie d\'activité', 'reference' : ['categorie_activite', 'responsable', 'type_activite', 'lieu']},
+                  'facture' : {'nom' : 'Facture', 'reference' : ['facture', 'participante']},
+                  'groupe' : {'nom' : 'Groupe', 'reference' : ['groupe', 'activite']},
+                  'inscription': {'nom' : 'Inscription', 'reference' : ['inscription', 'participante', 'activite']},
+                  'lieu' : {'nom' : 'Lieu', 'reference' : ['lieu']},
+                  'membre' :  {'nom' : 'Membre', 'reference' : ['membre', 'participante']},
+                  'participante' : {'nom' : 'Participante', 'reference' : ['participante']},
+                  'responsable' : {'nom' : 'Responsable', 'reference' : ['responsable']},
+                  'type_activite' : {'nom' : 'Type d\'activité', 'reference' : ['type_activite']}
+                  }
+
+    """
+    Valeur des types de donnée
+
+    Les types de donnée peuvent être soit des types standard "String", "Integer"... ou encore un type derivé de 
+    ceux-ci. Il sert à indiquer comment la donnée doit être traitée pour l'affichage ainsi que dans quel type de 
+    statistique il est possible de l'afficher. 
+    """
+    TYPE_STRING = 0
+    TYPE_INTEGER = 1
+    TYPE_DATE = 2
+    TYPE_TIME = 3
+    TYPE_BOOLEAN = 4
+    TYPE_DATETIME = 5
+    # Type pour l'affichage correct des prix
+    TYPE_PRIX = 6
+    # Type utilisé pour les status d'inscription seulement
+    # Nécessaire pour passer entiers de la base de donnée à une valeur compréhensible par l'utilisateur
+    TYPE_STATUS_INSCRIPTION = 7
+
+    """
+    Information sur la tables de la base de donnée
+
+    Ces listes servent à faire la correspondance entre les noms affichée et le nom réel dans la base de donnée. 
+    Lors de la création des ComboBox, le texte est la Value et les data est la Key
+
+    Les colonnes qui contiennent dans foreign key ne doivent pas être ajoutées à ces dictionnaires. 
+
+    Liste des colonnes pour chaque table de la base de donnée
+        Key : Nom de la table
+        Value : Nom pour l'affichage
+    """
+    # Colonne de la table activité
+    DICT_ACTIVITE = {
+                     'date' : {'nom' : 'Date', 'type' : TYPE_DATE},
+                     'heure_debut' : {'nom' : 'Heure de début', 'type' : TYPE_TIME},
+                     'heure_fin' : {'nom' : 'Heure de fin', 'type' : TYPE_TIME},
+                     'date_limite_inscription' : {'nom' : 'Date limite d\'inscription', 'type' : TYPE_DATE},
+                     'status' : {'nom' : 'Activité annulée', 'type' : TYPE_BOOLEAN}
+                    }
+
+    # Colonne de la table article
+    DICT_ARTICLE = {
+                    'prix': {'nom' : 'Prix', 'type' : TYPE_PRIX},
+                    'description' : {'nom' : 'Description', 'type' : TYPE_STRING}
+                   }
+
+    # Colonne de la table categorie d'activité
+    DICT_CATEGORIE_ACTIVITE = {
+                               'nom' : {'nom' : 'Nom', 'type' : TYPE_STRING},
+                               'prix_membre' : {'nom' : 'Prix membre', 'type' : TYPE_PRIX},
+                               'prix_non_membre' : {'nom' : 'Prix régulier', 'type' : TYPE_PRIX},
+                               'participante_minimum' : {'nom' : 'Nombre de participante minimum', 'type' : TYPE_INTEGER},
+                               'participante_maximum' : {'nom' : 'Nombre de participante maximum', 'type' : TYPE_INTEGER}
+                              }
+
+    # Colonne de la table facture
+    DICT_FACTURE = {
+                    'date' : {'nom' : 'Date', 'type' : TYPE_DATE},
+                    'numero_recu' : {'nom' : 'Numéro du reçu', 'type' : TYPE_INTEGER},
+                    'total' : {'nom' : 'Total', 'type' : TYPE_PRIX}
+                   }
+
+    # Colonne de la table groupe
+    DICT_GROUPE = {
+                   'f_0_4' : {'nom' : 'Femme 0-4 ans', 'type' : TYPE_INTEGER},
+                   'f_5_11' : {'nom' : 'Femme 5-11 ans', 'type' : TYPE_INTEGER},
+                   'f_12_17' : {'nom' : 'Femme 12-17 ans', 'type' : TYPE_INTEGER},
+                   'f_18_34' : {'nom' : 'Femme 18-34 ans', 'type' : TYPE_INTEGER},
+                   'f_35_64' : {'nom' : 'Femme 35-64 ans', 'type' : TYPE_INTEGER},
+                   'f_65' : {'nom' : 'Femme 65+ ans', 'type' : TYPE_INTEGER},
+                   'h_0_4' : {'nom' : 'Homme 0-4 ans', 'type' : TYPE_INTEGER},
+                   'h_5_11' : {'nom' : 'Homme 5-11 ans', 'type' : TYPE_INTEGER},
+                   'h_12_17' : {'nom' : 'Homme 12-17 ans', 'type' : TYPE_INTEGER},
+                   'h_18_34' : {'nom' : 'Homme 18-34 ans', 'type' : TYPE_INTEGER},
+                   'h_35_64' : {'nom' : 'Homme 35-64 ans', 'type' : TYPE_INTEGER},
+                   'h_65' : {'nom' : 'Homme 65+ ans', 'type' : TYPE_INTEGER}
+                  }
+
+    # Colonne de la table des inscriptions
+    DICT_INSCRIPTION = {
+                        'status' : {'nom' : 'Status', 'type' : TYPE_STATUS_INSCRIPTION},
+                        'present' : {'nom' : 'Présent', 'type' : TYPE_BOOLEAN},
+                        'time' : {'nom' : 'Date', 'type' : TYPE_DATE}
+                       }
+
+    # Colonne de la table des lieux
+    DICT_LIEU = {
+                  'nom' : {'nom' : 'Nom', 'type' : TYPE_STRING},
+                  'adresse_1' : {'nom' : 'Adresse ligne 1', 'type' : TYPE_STRING},
+                  'adresse_2' : {'nom' : 'Adresse ligne 2', 'type' : TYPE_STRING},
+                  'ville' : {'nom' : 'Ville', 'type' : TYPE_STRING},
+                  'province' : {'nom' : 'Province', 'type' : TYPE_STRING},
+                  'code_postal' : {'nom' : 'Code postal', 'type' : TYPE_STRING}
+                 }
+
+    # Colonne de la table des membres
+    DICT_MEMBRE = {
+                   'actif' : {'nom' : 'Actif', 'type' : TYPE_BOOLEAN},
+                   'numero_membre' : {'nom' : 'Numéro de membre', 'type' : TYPE_STRING},
+                   'membre_honoraire' : {'nom' : 'Membre honoraire', 'type' : TYPE_BOOLEAN},
+                   'date_renouvellement' : {'nom' : 'Date de renouvellement', 'type' : TYPE_DATE}
+                  }
+
+    # Colonne de la table des participantes
+    DICT_PARTICIPANTE = {
+                         'appellation' : {'nom' : 'Appellation', 'type' : TYPE_STRING},
+                         'prenom' : {'nom' : 'Prénom', 'type' : TYPE_STRING},
+                         'nom' : {'nom' : 'Nom', 'type' : TYPE_STRING},
+                         'adresse_1' : {'nom' : 'Adresse ligne 1', 'type' : TYPE_STRING},
+                         'adresse_2' : {'nom' : 'Adresse ligne 2', 'type' : TYPE_STRING},
+                         'ville' : {'nom' : 'Ville', 'type' : TYPE_STRING},
+                         'province' : {'nom' : 'Province', 'type' : TYPE_STRING},
+                         'code_postal' : {'nom' : 'Code postal', 'type' : TYPE_STRING},
+                         'courriel' : {'nom' : 'Adresse courriel', 'type' : TYPE_STRING},
+                         'telephone_1' : {'nom' : 'Téléphone 1', 'type' : TYPE_INTEGER},
+                         'telephone_2' : {'nom' : 'Téléphone 2', 'type' : TYPE_INTEGER},
+                         'poste_telephone_1' : {'nom' : 'Poste de téléphone 1', 'type' : TYPE_INTEGER},
+                         'poste_telephone_2' : {'nom' : 'Poste de téléphone 2', 'type' : TYPE_INTEGER},
+                         'date_naissance' : {'nom' : 'Année de naissance', 'type' : TYPE_INTEGER},
+                         'personne_nourrie' : {'nom' : 'Nombre de personne nourries', 'type' : TYPE_INTEGER},
+                         'consentement_photo' : {'nom' : 'Consentement photo', 'type' : TYPE_BOOLEAN}
+                        }
+
+    # Colonne de la table des responsables
+    DICT_RESPONSABLE = {
+                        'prenom' : {'nom' : 'Prénom', 'type' : TYPE_STRING},
+                        'nom' : {'nom' : 'Nom', 'type' : TYPE_STRING}
+                       }
+
+    # Colonne de la table du type d'activité
+    DICT_TYPE_ACTIVITE = {
+                          'nom' : {'nom' : 'Nom', 'type' : TYPE_STRING}
+                         }
+
     def __init__(self, database):
         super(Statistiques, self).__init__()
         ui = os.path.join(os.path.dirname(__file__), 'GUI', 'statistique.ui')
@@ -55,8 +206,14 @@ class Statistiques(Form):
         # Slots
         self.btn_annuler.clicked.connect(self.reject)
 
+    """
+    Gestion de la table des champs
+    """
+
     def ajouter_ligne_champ(self):
         """Ajouter une ligne au tableau des champs"""
+
+        # Ajouter une ligne à la table
         self.tbl_champs.insertRow(self.tbl_champs.rowCount())
         r = self.tbl_champs.rowCount() - 1
 
@@ -325,26 +482,3 @@ class Statistiques(Form):
         liste_table.sort()
         self.DICT_TABLE = dict(zip(liste_table, liste_table))
 
-        # Changer le nom des éléments du dictionnaire
-        if 'activite' in self.DICT_TABLE:
-            self.DICT_TABLE['activite'] = 'Activité'
-        if 'article' in self.DICT_TABLE:
-            self.DICT_TABLE['article'] = 'Article'
-        if 'categorie_activite' in self.DICT_TABLE:
-            self.DICT_TABLE['categorie_activite'] = 'Catégorie d\'activité'
-        if 'facture' in self.DICT_TABLE:
-            self.DICT_TABLE['facture'] = 'Facture'
-        if 'groupe' in self.DICT_TABLE:
-            self.DICT_TABLE['groupe'] = 'Groupe'
-        if 'inscription' in self.DICT_TABLE:
-            self.DICT_TABLE['inscription'] = 'Inscription'
-        if 'lieu' in self.DICT_TABLE:
-            self.DICT_TABLE['lieu'] = 'Lieu'
-        if 'membre' in self.DICT_TABLE:
-            self.DICT_TABLE['membre'] = 'Membre'
-        if 'participante' in self.DICT_TABLE:
-            self.DICT_TABLE['participante'] = 'Participante'
-        if 'responsable' in self.DICT_TABLE:
-            self.DICT_TABLE['responsable'] = 'Responsable'
-        if 'type_activite' in self.DICT_TABLE:
-            self.DICT_TABLE['type_activite'] = 'Type d\'activité'
